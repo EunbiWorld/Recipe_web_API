2199fc7476687b294dda05b1b1350a817978ae90
EunbiWorld <rkql1304@naver.com>
2023-11-16 02:19:32 +0900

    recommendation 페이지에 레시피 4개랜덤으로 추천 기능 넣음

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express();
 
 const recipe_api = require("./recipe_API_JSON.json");
 
+// Fisher-Yates 알고리즘을 사용하여 배열을 무작위로 섞음
+function shuffleRecipes() {
+  for (let i = recipe_api.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [recipe_api[i], recipe_api[j]] = [recipe_api[j], recipe_api[i]];
+  }
+}
+
+// 서버가 시작될 때마다 레시피 데이터를 무작위로 섞음
+shuffleRecipes();
+
+// 24시간마다 레시피 데이터를 무작위로 섞음
+setInterval(shuffleRecipes, 24 * 60 * 60 * 1000);
+
 const server = app.listen(3000, function () {
   const host = server.address().address;
   const port = server.address().port;
@@ -20,4 +34,9 @@ app.get("/", (req, res) => {
 // 이 경로로 요청이 오면 'recipe_API_JSON.json' 파일의 내용을 JSON 형식으로 반환
 app.get("/api/recipe", (req, res) => {
   res.json(recipe_api);
+});
+
+// 무작위로 선택된 4개의 레시피만 반환하는 API
+app.get("/api/recommendation", (req, res) => {
+  res.json(recipe_api.Food['Food recipe'].slice(0, 4));
 });
