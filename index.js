4251babfee300d6f8b62b89694aa05d96057a68c
EunbiWorld <rkql1304@naver.com>
2023-11-15 23:35:06 +0900

    node.js와 express 설치 후 기본 틀 만 잡음

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const recipe_api = require('./recipe_API_JSON.json');
+  
+const server = app.listen(3000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  
+  console.log('Server is working : PORT - ',port);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/get_recipe', (req, res) => {
+    res.json(recipe_api);
+} );
